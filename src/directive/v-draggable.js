05fa192bb89510b6cd6e9832fe698514b206caff
Larry <shaka422@hotmail.com>
2022-04-07 23:58:19 +0800

    bugTest

--- src/directive/v-draggable.js
+++ src/directive/v-draggable.js
@@ -1,16 +1,27 @@
 import Sortable from "sortablejs";
-function deepClone(cloneData) {
-  return JSON.parse(JSON.stringify(cloneData));
-}
+// import Store from "../store/index";
+// function deepClone(cloneData) {
+//   return JSON.parse(JSON.stringify(cloneData));
+// }
 const draggabletable = {
-  mounted(el, binding) {
+  updated(el, binding, vnode) {
+    console.log("drag");
+    // console.log(vnode.dirs[0].instance.tableData);
+    // const data = binding.value.get;
+
     Sortable.create(el.querySelector(".el-table__body tbody"), {
       animation: binding.value.animation,
       onEnd: (evt) => {
-        const data = deepClone(binding.instance.$data);
-        const dragItem = data.tableData.splice(evt.oldIndex, 1)[0];
-        data.tableData.splice(evt.newIndex, 0, dragItem);
-        binding.value.saveData(data.tableData);
+        const data = binding.instance.tmp;
+
+        const dragItem = data.splice(evt.oldIndex, 1)[0];
+        data.splice(evt.newIndex, 0, dragItem);
+        binding.value.saveData(data);
+
+        // binding.value.saveData([
+        //   ...data.slice(0, evt.newIndex),
+        //   ...data.slice(evt.newIndex),
+        // ]);
       },
     });
   },
