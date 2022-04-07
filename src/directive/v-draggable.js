1231a5221013ddf8cb6630bb6e708a828b65c2c4
larry-du <58778998+larry-du@users.noreply.github.com>
2022-04-07 18:10:23 +0800

    Update v-draggable.js
    add deepClone function

--- src/directive/v-draggable.js
+++ src/directive/v-draggable.js
@@ -1,11 +1,13 @@
 import Sortable from "sortablejs";
-
+function deepClone(cloneData) {
+  return JSON.parse(JSON.stringify(cloneData));
+}
 const draggabletable = {
   mounted(el, binding) {
     Sortable.create(el.querySelector(".el-table__body tbody"), {
       animation: binding.value.animation,
       onEnd: (evt) => {
-        const data = binding.instance.$data;
+        const data = deepClone(binding.instance.$data);
         const dragItem = data.tableData.splice(evt.oldIndex, 1)[0];
         data.tableData.splice(evt.newIndex, 0, dragItem);
         binding.value.saveData(data.tableData);
